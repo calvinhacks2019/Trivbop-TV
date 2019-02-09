0382e77f8f6d088bbce5fc6245429a8841f2c8db
Austin Evans <evansau1@msu.edu>
2019-02-08 19:10:43 -0500

    Initial Commit

---
+++ Trivbop Mobile/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Trivbop Mobile
+//
+//  Created by Austin Evans on 2/8/19.
+//  Copyright © 2019 CalvinHacks2019. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
